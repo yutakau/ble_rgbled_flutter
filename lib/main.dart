c45ff11b272a2286f5e8514dbede0dcc2488b089
yutakau <yutakau@gmail.com>
2023-06-24 14:55:40 +0900

    Added permission check on Android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'device.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 void main() {
-  runApp(const MyApp());
+  if (Platform.isAndroid) {
+   WidgetsFlutterBinding.ensureInitialized();
+   [
+     Permission.location,
+     Permission.bluetooth,
+     Permission.bluetoothConnect,
+     Permission.bluetoothScan
+   ].request().then((status) {
+     runApp(const MyApp());
+   });
+  } else {
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
